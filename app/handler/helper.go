0a027028bb3a26b6145f9782f7276b03c5f62291
七夏浅笑 <16562294+julydate@users.noreply.github.com>
2023-11-18 18:59:13 +0800

    Revert "Revert "Refactor many things""

---
+++ app/handler/helper.go
@@ -0,0 +1,53 @@
+package handler
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"golang.org/x/net/idna"
+)
+
+func checkValue(response http.ResponseWriter, form url.Values, key string) (string, error) {
+	if len(form.Get(key)) == 0 {
+		response.WriteHeader(400)
+		fmt.Fprintf(response, "no %s specified.", key)
+		return "", fmt.Errorf("no %s specified", key)
+	}
+
+	return form.Get(key), nil
+}
+
+func handleErrorResponse(response http.ResponseWriter, ip string, statusCode int, errorMessage, printMessage string) {
+	logrus.Infof("Access from IP: %s", ip)
+	logrus.Infof(printMessage)
+	response.WriteHeader(statusCode)
+	fmt.Fprintf(response, errorMessage)
+}
+
+func validateFileAndDomain(ip string, domain string, file string, response http.ResponseWriter) bool {
+	baseDir := "certs"
+	domain = sanitizedDomain(domain)
+	if _, err := os.Stat(path.Join(baseDir, domain)); os.IsNotExist(err) {
+		handleErrorResponse(response, ip, 404, "Domain not exist.", fmt.Sprintf("Incoming illegal domain: %s", domain))
+		return false
+	}
+	if _, err := os.Stat(path.Join(baseDir, domain, "certificates", file)); os.IsNotExist(err) {
+		handleErrorResponse(response, ip, 404, "File not exist.", fmt.Sprintf("Incoming illegal filename: %s", file))
+		return false
+	}
+	return true
+}
+
+func sanitizedDomain(domain string) string {
+	safe, err := idna.ToASCII(strings.ReplaceAll(domain, "*", "_"))
+	if err != nil {
+		log.Panic(err)
+	}
+	return safe
+}
---
+++ app/mylego/mylego.go
@@ -0,0 +1,158 @@
+package mylego
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+var defaultPath string
+
+func New(certConf *CertConfig) (*LegoCMD, error) {
+	// Set default path to configPath/cert
+	var p = "."
+	if cwd, err := os.Getwd(); err == nil {
+		p = cwd
+	}
+
+	defaultPath = filepath.Join(p, "certs", sanitizedDomain(certConf.CertDomain))
+	lego := &LegoCMD{
+		Conf: certConf,
+		path: defaultPath,
+	}
+
+	return lego, nil
+}
+
+func (l *LegoCMD) getPath() string {
+	return l.path
+}
+
+func (l *LegoCMD) getCertConfig() *CertConfig {
+	return l.Conf
+}
+
+// DNSCert cert a domain using DNS API
+func (l *LegoCMD) DNSCert() (CertPath string, KeyPath string, err error) {
+	defer func() (string, string, error) {
+		// Handle any error
+		if r := recover(); r != nil {
+			switch x := r.(type) {
+			case string:
+				err = errors.New(x)
+			case error:
+				err = x
+			default:
+				err = errors.New("unknown panic")
+			}
+			return "", "", err
+		}
+		return CertPath, KeyPath, nil
+	}()
+
+	// Set Env for DNS configuration
+	for key, value := range l.Conf.DNSEnv {
+		os.Setenv(strings.ToUpper(key), value)
+	}
+
+	// First check if the certificate exists
+	CertPath, KeyPath, err = checkCertFile(l.Conf.CertDomain)
+	if err == nil {
+		return CertPath, KeyPath, err
+	}
+
+	err = l.Run()
+	if err != nil {
+		return "", "", err
+	}
+	CertPath, KeyPath, err = checkCertFile(l.Conf.CertDomain)
+	if err != nil {
+		return "", "", err
+	}
+	return CertPath, KeyPath, nil
+}
+
+// HTTPCert cert a domain using http methods
+func (l *LegoCMD) HTTPCert() (CertPath string, KeyPath string, err error) {
+	defer func() (string, string, error) {
+		// Handle any error
+		if r := recover(); r != nil {
+			switch x := r.(type) {
+			case string:
+				err = errors.New(x)
+			case error:
+				err = x
+			default:
+				err = errors.New("unknown panic")
+			}
+			return "", "", err
+		}
+		return CertPath, KeyPath, nil
+	}()
+
+	// First check if the certificate exists
+	CertPath, KeyPath, err = checkCertFile(l.Conf.CertDomain)
+	if err == nil {
+		return CertPath, KeyPath, err
+	}
+
+	err = l.Run()
+	if err != nil {
+		return "", "", err
+	}
+
+	CertPath, KeyPath, err = checkCertFile(l.Conf.CertDomain)
+	if err != nil {
+		return "", "", err
+	}
+
+	return CertPath, KeyPath, nil
+}
+
+// RenewCert renew a domain cert
+func (l *LegoCMD) RenewCert() (CertPath string, KeyPath string, ok bool, err error) {
+	defer func() (string, string, bool, error) {
+		// Handle any error
+		if r := recover(); r != nil {
+			switch x := r.(type) {
+			case string:
+				err = errors.New(x)
+			case error:
+				err = x
+			default:
+				err = errors.New("unknown panic")
+			}
+			return "", "", false, err
+		}
+		return CertPath, KeyPath, ok, nil
+	}()
+
+	ok, err = l.Renew()
+	if err != nil {
+		return
+	}
+
+	CertPath, KeyPath, err = checkCertFile(l.Conf.CertDomain)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func checkCertFile(domain string) (string, string, error) {
+	keyPath := path.Join(defaultPath, baseCertificatesFolderName, fmt.Sprintf("%s.key", domain))
+	certPath := path.Join(defaultPath, baseCertificatesFolderName, fmt.Sprintf("%s.crt", domain))
+	if _, err := os.Stat(keyPath); os.IsNotExist(err) {
+		return "", "", fmt.Errorf("cert key failed: %s", domain)
+	}
+	if _, err := os.Stat(certPath); os.IsNotExist(err) {
+		return "", "", fmt.Errorf("cert cert failed: %s", domain)
+	}
+	absKeyPath, _ := filepath.Abs(keyPath)
+	absCertPath, _ := filepath.Abs(certPath)
+	return absCertPath, absKeyPath, nil
+}
